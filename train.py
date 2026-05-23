d0540cc6d8a803688c39cafe2be63152e1889620
Shuvo <suvo10.info@gmail.com>
2026-05-23 11:46:05 +0600

    fdik

--- train.py
+++ train.py
@@ -12,4 +12,6 @@ y=df['Survived']
 
 #split the dataset 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model=RandomForestClassifier(n_estimators=100,random_state=42)
+model.fit(X_train,y_train)
 
