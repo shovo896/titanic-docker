223f48af5a4449fb1611d6ac60ae1b9a0ec5669a
Shuvo <suvo10.info@gmail.com>
2026-05-23 11:44:52 +0600

    wdkiji

---
+++ train.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+print("Loading data...")
+url="https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df=pd.read_csv(url)
+features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+X=df[features]
+y=df['Survived']
+
+#split the dataset 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
