f8805d1d7f1c7f4cca03cd07239f156f1c24e12d
Shuvo <suvo10.info@gmail.com>
2026-05-23 12:02:52 +0600

    eg

--- train.py
+++ train.py
@@ -15,3 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model=RandomForestClassifier(n_estimators=100,random_state=42)
 model.fit(X_train,y_train)
 
+predictions=model.predict(X_test)
+accuracy=accuracy_score(y_test,predictions)
+print(f"Model Accuracy: {accuracy:.2f}")
+
